6a6088c5d61b167c44b10979a96fef0dffaa3a7c
James Picone <jamesmpicone+github@gmail.com>
2016-09-01 22:57:54 +0930

    table_ whatever

--- rules.mk
+++ rules.mk
@@ -1,11 +1,11 @@
 .PHONY: all
 .PHONY: clean
 
-%.html cards_%.html : %.xml table.xslt cards.xslt
-	saxonb-xslt -s:"$<" -o:"cards_$(<:.xml=.html)" -xsl:cards.xslt
-	saxonb-xslt -s:"$<" -o:"$(<:.xml=.html)" -xsl:table.xslt
+table_%.html card_%.html : %.xml table.xslt cards.xslt
+	saxonb-xslt -s:"$<" -o:"card_$(<:.xml=.html)" -xsl:cards.xslt
+	saxonb-xslt -s:"$<" -o:"table_$(<:.xml=.html)" -xsl:table.xslt
 	
-html = $(patsubst %.xml,%.html,$(wildcard *.xml)) $(patsubst %.xml,card_%.html,$(wildcard *.xml))
+html = $(patsubst %.xml,table_%.html,$(wildcard *.xml)) $(patsubst %.xml,card_%.html,$(wildcard *.xml))
 
 all : $(html)
 clean :
