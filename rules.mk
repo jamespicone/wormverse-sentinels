5f7841641a4de339cc1756a58ac1995c97fce510
James Picone <jamesmpicone+github@gmail.com>
2016-10-09 12:37:18 +1030

    Fix makefile cards/card mixup

--- rules.mk
+++ rules.mk
@@ -6,7 +6,7 @@ table_%.html card_%.html : %.xml table.xslt cards.xslt
 	saxonb-xslt -s:"$<" -o:"card_$(<:.xml=.html)" -xsl:cards.xslt
 	saxonb-xslt -s:"$<" -o:"table_$(<:.xml=.html)" -xsl:table.xslt
 	
-html = $(patsubst %.xml,table_%.html,$(wildcard *.xml)) $(patsubst %.xml,cards_%.html,$(wildcard *.xml))
+html = $(patsubst %.xml,table_%.html,$(wildcard *.xml)) $(patsubst %.xml,card_%.html,$(wildcard *.xml))
 
 all : $(html)
 clean :
