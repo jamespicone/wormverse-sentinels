4046e6e175b5bf8728fb003af0fdac6c0d418c29
james <james@localhost>
2017-01-09 00:39:45 +1030

    More makefile fuckery

--- osdep.mk
+++ osdep.mk
@@ -3,17 +3,23 @@ ifeq ($(OS),Windows_NT)
 	transform := Transform
 endif
 
-delete := rm -f
-ifeq ($(OS),Windows_NT)
-	delete := del /f
-endif
-
-mkdir := mkdir -p
-ifeq ($(OS),Windows_NT)
-	mkdir := mkdir
-endif
-
 copy := cp
 ifeq ($(OS),Windows_NT)
-	copy := copy
+	copy := xcopy
+endif
+
+package_folder := ../Cards/$(folder)/
+
+ifeq ($(OS),Windows_NT)
+   mkdir = mkdir $(subst /,\,$(1)) > nul 2>&1 || (exit 0)
+   rm = $(wordlist 2,65535,$(foreach FILE,$(subst /,\,$(1)),& del $(FILE) > nul 2>&1)) || (exit 0)
+   rmdir = rmdir $(subst /,\,$(1)) > nul 2>&1 || (exit 0)
+   echo = echo $(1)
+   copy = robocopy . $(2) $(1) > nul || (exit 0)
+else
+   mkdir = mkdir -p $(1)
+   rm = rm $(1) > /dev/null 2>&1 || true
+   rmdir = rmdir $(1) > /dev/null 2>&1 || true
+   echo = echo "$(1)"
+   copy = cp $(1) $(2)
 endif
