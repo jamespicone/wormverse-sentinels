e23a1439c1f9851e3faa3743fca1d91d857f2aa7
James Picone <jamesmpicone+github@gmail.com>
2017-04-06 22:45:06 +0930

    Makefile rules patch to generate card images for tabletop simulator

--- rules.mk
+++ rules.mk
@@ -8,11 +8,16 @@ table_%.html card_%.html : %.xml table.xslt cards.xslt ../card.xslt
 	$(transform) -s:"$<" -o:"card_$(<:.xml=.html)" -xsl:cards.xslt
 	$(transform) -s:"$<" -o:"table_$(<:.xml=.html)" -xsl:table.xslt
 	
+image_%.png : card_%.html ../cards.css
+	cutycapt --url="$<" --out=$@ --min-width=1439
+	
 html = $(patsubst %.xml,table_%.html,$(wildcard *.xml)) $(patsubst %.xml,card_%.html,$(wildcard *.xml))
+image = $(patsubst %.xml,image_%.png,$(wildcard *.xml))
 
-all : $(html)
+all : $(html) $(image)
 clean :
 	$(call rm, $(html))
+	$(call rm, $(image))
 	
 package :
 	$(call mkdir,$(package_folder))
